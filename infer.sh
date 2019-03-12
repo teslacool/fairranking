bda140c5098a8598465a2bf25f01880869adc836
Tesla <1462540095@qq.com>
2019-03-12 09:18:13 +0000

    some errors

--- infer.sh
+++ infer.sh
@@ -23,7 +23,7 @@ grep ^T $outputfile | cut -f2- > "$outputfile".ref
 
 grep ^S $outputfile | cut -f2- > "$outputfile".src
 
-if [ "$needdebpe" = "true" ]
+if [ "$needdetc" = "true" ]
 then
     echo "need detruecase.."
     if [ ! -d $scripts ]
