efdd10cb9d6f72294e937738ced0b3d004528cb9
Tesla <1462540095@qq.com>
2019-03-12 13:33:59 +0000

    cal bleu

---
+++ cal.py
@@ -0,0 +1,84 @@
+import numpy as np
+import sys
+import os
+
+
+ckt1 = sys.argv[1]
+ckt2 = sys.argv[2]
+lng = sys.argv[3]
+year = sys.argv[4]
+input_1 = ckt1 + '.output.sys'
+input_2 = ckt2 + '.output.sys'
+score_11 = ckt1 + '.output.{}'.format(0)
+score_12 = ckt1 + '.output.{}'.format(1)
+score_21 = ckt2 + '.output.{}'.format(1)
+score_22 = ckt2 + '.output.{}'.format(0)
+
+tmpF = ckt1 +'.result.alpha{}.gamma{}'
+
+
+
+beamsize = 5
+detokenize = 'perl /blob/v-jinhzh/code/mosesdecoder/scripts/tokenizer/detokenizer.perl -l {} < {} > {}'
+sacrebleu = 'cat {} | /blob/v-jinhzh/code/sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t {} -l {}'
+
+
+def read_file(fname):
+    with open(fname, "r", encoding="utf8") as ff:
+        all_lines = [x.strip() for x in ff]
+    return all_lines, np.array([len(x.split()) for x in all_lines]).astype("float32")
+
+
+def read_score(fname):
+    all_lines, _ = read_file(fname)
+    return np.array([float(x) for x in all_lines]).astype("float32")
+
+
+def process_one_file(lines, cnt, S1, S2, alpha, gamma):
+    SS = alpha * S1 + (1. - alpha) * S2
+    SS = SS / (cnt + 1.) ** gamma
+    SS = SS.reshape((-1, beamsize))
+    idx_list = SS.argmax(axis=1).tolist()
+    ret_lines, ret_scores = [], []
+    for ii, idx in enumerate(idx_list):
+        ret_lines.append(lines[ii * beamsize + idx])
+        ret_scores.append(SS[ii, idx])
+
+    return ret_lines, ret_scores
+
+
+def find_winner(ret_lines_1, ret_lines_2, score_1, score_2):
+    assert len(ret_lines_1) == len(ret_lines_2) == len(score_1) == len(score_2)
+    return [l1 if s1 > s2 else l2 for (l1, l2, s1, s2) in zip(ret_lines_1, ret_lines_2, score_1, score_2)]
+
+
+all_lines_1, cnt_1 = read_file(input_1)
+all_lines_2, cnt_2 = read_file(input_2)
+
+score_11 = read_score(score_11)
+score_12 = read_score(score_12)
+
+score_21 = read_score(score_21)
+score_22 = read_score(score_22)
+
+gamma = -0.4
+
+max_bin = 20
+
+for aa in range(max_bin + 1):
+    alpha = aa / max_bin
+    alpha = 1. - alpha
+    ret_lines_1, ret_scores_1 = process_one_file(all_lines_1, cnt_1, score_11, score_12, alpha, gamma)
+    ret_lines_2, ret_scores_2 = process_one_file(all_lines_2, cnt_2, score_22, score_21, alpha, gamma)
+
+    winner_lines = find_winner(ret_lines_1, ret_lines_2, ret_scores_1, ret_scores_2)
+    newf = tmpF.format(alpha, gamma)
+    with open(newf, "w", encoding="utf8") as fw:
+        for x in winner_lines:
+            print(x, file=fw)
+    tgtlng = lng[-2:]
+    newcmd = detokenize.format(tgtlng, newf, newf + 'detok')
+    os.system(newcmd)
+    newcmd = sacrebleu.format(newf + 'detok', year, lng)
+    os.system(newcmd)
+
--- reranking.sh
+++ reranking.sh
@@ -21,6 +21,7 @@ datapath2=/blob/v-jinhzh/data/bt02/warmnmtdata02/en2ru
 bpetc2=/blob/v-jinhzh/data/bpetc/cold
 needtc2=true
 
+year=wmt18
 
 echo "bash infer.sh $gpuid $ckt1 $datapath1 $needtc1
 bash infer.sh $gpuid $ckt2 $datapath2 $needtc2"
@@ -39,3 +40,4 @@ bash eval.sh $gpuid $datapath2 $ckt2 $ckt1 1 $bpetc2 $needtc2 $src $tgt
 bash eval.sh $gpuid $datapath2 $ckt2 $ckt2 0 $bpetc2 $needtc2 $src $tgt
 bash eval.sh $gpuid $datapath1 $ckt1 $ckt2 1 $bpetc1 $needtc1 $src $tgt
 
+python cal.py $ckt1 $ckt2 ${src}-${tgt} $year
