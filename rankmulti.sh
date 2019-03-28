a169b27b45127c6d75dd6fd83c2f0d97f68da729
Tesla <1462540095@qq.com>
2019-03-28 13:37:36 +0000

    rm more

--- rankmulti.sh
+++ rankmulti.sh
@@ -116,4 +116,8 @@ python multical.py $beamsize
 echo ">>> perl ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $tgt < output.tok > output.tok.detok"
 perl ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $tgt < output.tok > output.tok.detok
 echo ">>> cat output.tok.detok | ../sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t wmt${year:2:2} -l ${src}-${tgt}"
-cat output.tok.detok | ../sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t wmt${year:2:2} -l ${src}-${tgt}
+ cat output.tok.detok | ../sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t wmt${year:2:2} -l ${src}-${tgt}
+rm input.tok
+rm output.sys
+rm output.score
+rm output.tok
