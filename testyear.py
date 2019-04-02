a5b1deb4850c9e39851ca6abc06ac55c3faed123
Tesla <1462540095@qq.com>
2019-04-02 03:26:12 +0000

    testyear.py

---
+++ testyear.py
@@ -0,0 +1,45 @@
+import argparse
+import glob
+import os
+parser = argparse.ArgumentParser()
+parser.add_argument('pair', type=str)
+parser.add_argument('year', type=int)
+parser.add_argument('bpetc', type=str)
+parser.add_argument('dictdir', type=str)
+parser.add_argument('cktdir', type=str)
+parser.add_argument('-l', type=float, default=1.)
+parser.add_argument('-b', type=int, default=5)
+parser.add_argument('--r2l', action='store_true')
+parser.add_argument('--num', type=int, default=10)
+args = parser.parse_args()
+pair = args.pair
+year = args.year
+bpetc = args.bpetc
+dictdir = args.dictdir
+num = args.num
+cmd = 'bash testyear.sh {} {} {} {}'.format(pair, year, bpetc, dictdir)
+cktdir = args.cktdir
+lenpen =  args.l
+beamsize = args.b
+r2l = '--r2l' if args.r2l else ''
+finalcmd = '{cmd} {cktpath} -b {beamsize} -l {lenpen} {r2l}'
+
+ckts = glob.glob(os.path.join(cktdir, 'checkpoint_*_*.pt'))
+def ckt2iter(ckts):
+    dict_ckt2iter = []
+    for ckt in ckts:
+        iter = int(ckt.split('_')[-1][:-3])
+        dict_ckt2iter.append((iter, ckt))
+    return dict_ckt2iter
+def select(dict_ckt2iter):
+    sorteddict = sorted(dict_ckt2iter, reverse=True)
+    x = [x[1] for x in sorteddict]
+    return x
+dict_ckt2iter = ckt2iter(ckts)
+ckts = select(dict_ckt2iter)[:num]
+print(ckts)
+
+for ckt in ckts:
+    newcmd = finalcmd.format(cmd=cmd, cktpath = ckt, beamsize=beamsize, lenpen=lenpen, r2l=r2l)
+    print(newcmd)
+    os.system(newcmd)
