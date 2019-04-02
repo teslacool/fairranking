4a273ae7b490a7cd08943c20ad93104f7ece7a9a
Tesla <1462540095@qq.com>
2019-04-02 08:03:44 +0000

    testyear with epoch

--- testyear.py
+++ testyear.py
@@ -1,6 +1,7 @@
 import argparse
 import glob
 import os
+import re
 parser = argparse.ArgumentParser()
 parser.add_argument('pair', type=str)
 parser.add_argument('year', type=int)
@@ -11,12 +12,14 @@ parser.add_argument('-l', type=float, default=1.)
 parser.add_argument('-b', type=int, default=5)
 parser.add_argument('--r2l', action='store_true')
 parser.add_argument('--num', type=int, default=10)
+parser.add_argument('--epoch',action='store_true' )
 args = parser.parse_args()
 pair = args.pair
 year = args.year
 bpetc = args.bpetc
 dictdir = args.dictdir
 num = args.num
+isepoch = args.epoch
 cmd = 'bash testyear.sh {} {} {} {}'.format(pair, year, bpetc, dictdir)
 cktdir = args.cktdir
 lenpen =  args.l
@@ -24,12 +27,18 @@ beamsize = args.b
 r2l = '--r2l' if args.r2l else ''
 finalcmd = '{cmd} {cktpath} -b {beamsize} -l {lenpen} {r2l}'
 
-ckts = glob.glob(os.path.join(cktdir, 'checkpoint_*_*.pt'))
+ckts = glob.glob(os.path.join(cktdir, 'checkpoint_*_*.pt')) if not isepoch else glob.glob(os.path.join(cktdir, 'checkpoint*.pt'))
 def ckt2iter(ckts):
     dict_ckt2iter = []
     for ckt in ckts:
-        iter = int(ckt.split('_')[-1][:-3])
-        dict_ckt2iter.append((iter, ckt))
+        if not isepoch:
+            iter = int(ckt.split('_')[-1][:-3])
+            dict_ckt2iter.append((iter, ckt))
+        else:
+            match = re.match(os.path.join(cktdir, 'checkpoint\d+.pt'), ckt)
+            if match:
+                iter = int(match.group().split('checkpoint')[-1][:-3])
+                dict_ckt2iter.append((iter, ckt))
     return dict_ckt2iter
 def select(dict_ckt2iter):
     sorteddict = sorted(dict_ckt2iter, reverse=True)
@@ -42,4 +51,4 @@ print(ckts)
 for ckt in ckts:
     newcmd = finalcmd.format(cmd=cmd, cktpath = ckt, beamsize=beamsize, lenpen=lenpen, r2l=r2l)
     print(newcmd)
-    os.system(newcmd)
+    # os.system(newcmd)
