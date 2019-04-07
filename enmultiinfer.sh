52a8f3b59cbe2e5f0ccfd69e847cb1a32bdbf598
Tesla <1462540095@qq.com>
2019-04-07 02:58:31 +0000

    ensemblereranking

---
+++ enmultiinfer.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=0
+ckt=$1
+datapath=$2
+bpetc=$3
+year=$4
+srclng=$5
+lenpen=$6
+beamsize=$7
+scripts=$SCRIPTS
+bsz=$8
+
+
+bpetcdir=/blob/v-jinhzh/data/bpetc/$bpetc
+if [ ! -d ${bpetcdir}/model ]
+then
+    needdetc=false
+else
+    needdetc=true
+    tcdir=$bpetcdir/model
+    echo ">>> tcdir $tcdir"
+fi
+bpedir=$bpetcdir/bpe
+echo ">>> bpedir $bpedir"
+
+outputfile=ensembleoutput
+if [ -f $outputfile ]
+then
+    rm $outputfile
+fi
+srcfile=/blob/v-jinhzh/data/wmttest/testdata/test.${year}.${srclng}.tok
+export PYTHONIOENCODING="UTF-8"
+
+APPLY=$scripts/recaser/truecase.perl
+if [ "$needdetc" == true ]
+then
+    echo ">>> $APPLY -model ${tcdir}/tc.${srclng} < $srcfile > input.tok"
+    $APPLY -model ${tcdir}/tc.${srclng} < $srcfile > input.tok
+else
+    echo ">>> cp $srcfile input.tok"
+    cp $srcfile input.tok
+fi
+BPEROOT=/blob/v-jinhzh/code/subword-nmt/subword_nmt
+echo ">>> python $BPEROOT/apply_bpe.py -c $bpedir/$srclng.codes < input.tok > input.tok.bpe"
+python $BPEROOT/apply_bpe.py -c $bpedir/$srclng.codes < input.tok > input.tok.bpe
+
+echo ">>> cat input.tok.bpe | python interactive.py $datapath \
+--path $ckt --buffer-size 1024 \
+--batch-size $bsz --beam $beamsize --nbest  $beamsize  --remove-bpe  --lenpen $lenpen > $outputfile"
+cat input.tok.bpe | python interactive.py $datapath \
+--path $ckt --buffer-size 1024 \
+--batch-size $bsz --beam $beamsize --nbest  $beamsize  --remove-bpe  --lenpen $lenpen > $outputfile
+
+
+echo ">>> grep ^H $outputfile | cut -f3- > $outputfile.sys"
+grep ^H $outputfile | cut -f3- > $outputfile.sys
+echo ">>> rm $outputfile"
+rm $outputfile
+
+if [ $bpetc == "r2l" ]
+then
+    echo ">>> python reversesentence.py $outputfile.sys"
+    python reversesentence.py $outputfile.sys
+    echo ">>> mv $outputfile.sys.reversed $outputfile.sys"
+    mv $outputfile.sys.reversed $outputfile.sys
+fi
+
+if [ "$needdetc" = "true" ]
+then
+    echo ">>> need detruecase.."
+    if [ ! -d $scripts ]
+    then
+        echo "set your mose dir"
+        exit
+    fi
+    echo ">>> perl ${scripts}/recaser/detruecase.perl < $outputfile.sys > $outputfile.sys.tmp"
+    perl ${scripts}/recaser/detruecase.perl < "$outputfile".sys > "$outputfile".sys.tmp
+    echo ">>> mv $outputfile.sys.tmp $outputfile.sys"
+    mv $outputfile.sys.tmp $outputfile.sys
+fi
+
---
+++ ensemblereranking.sh
@@ -0,0 +1,141 @@
+#!/usr/bin/env bash
+HOME=/blob/v-jinhzh/code/fairranking
+cd $HOME
+
+bperoot=/blob/v-jinhzh/code/subword-nmt/subword_nmt
+export BPEROOT=$bperoot
+scripts=/blob/v-jinhzh/code/mosesdecoder/scripts
+export SCRIPTS=$scripts
+
+if [ $1 == '-h' ]
+then
+echo "bash  rankmulti.sh ru2en 2018 1. 5 ckt1 datapath1 bpetc1 bsz1 -  ckt2 datapath2 bpetc2 ... "
+exit
+fi
+pair=$1
+src=${pair:0:2}
+tgt=${pair:3:2}
+echo ">>> srclng $src"
+echo ">>> tgtlng $tgt"
+shift
+year=$1
+echo ">>> year $year"
+shift
+
+lenpen=$1
+echo ">>> lenpen $lenpen"
+shift
+beamsize=$1
+echo ">>> beamsize $beamsize"
+shift
+
+
+
+
+ensembleid=0
+while [ ! "$1" == '-' ]
+do
+enckts[$ensembleid]=$1
+shift
+endatapaths[$ensembleid]=$1
+shift
+enbpetcs[$ensembleid]=$1
+shift
+enbszs[$ensembleid]=$1
+shift
+echo ">>> enckt ${enckts[$ensembleid]} endatapath ${endatapaths[$ensembleid]} enbpetc ${enbpetcs[$ensembleid]} enbsz  ${enbszs[$ensembleid]}"
+ensembleid=$[ensembleid+1]
+done
+
+tmp=$#
+tmp=$[tmp%3]
+if [ $tmp != 0 ]
+then
+echo "args number must be divisible by 3"
+exit
+fi
+id=0
+while [[ $# > 0 ]]
+do
+ckts[$id]=$1
+shift
+datapaths[$id]=$1
+shift
+bpetcs[$id]=$1
+shift
+echo ">>> ckt ${ckts[$id]} datapath ${datapaths[$id]} bpetc ${bpetcs[$id]} "
+id=$[id+1]
+done
+
+totalcktnum=$id
+stopcnt=$[totalcktnum-1]
+enstopcnt=$[ensembleid-1]
+for id in `seq 0 $enstopcnt`
+do
+    echo ">>> bash enmultiinfer.sh ${enckts[$id]} ${endatapaths[$id]} ${enbpetcs[$id]} $year $src  $lenpen $beamsize ${enbszs[$id]} "
+    bash enmultiinfer.sh ${enckts[$id]} ${endatapaths[$id]} ${enbpetcs[$id]} $year $src  $lenpen $beamsize ${enbszs[$id]}
+    echo ">>> ensembleoutput ensembleoutput.$id"
+    mv ensembleoutput.sys ensembleoutput.sys.$id
+done
+if [ -f input.tok ]
+then
+    echo ">>> rm input.tok"
+    rm input.tok
+fi
+if [ -f output.sys ]
+then
+    echo ">>> rm output.sys"
+    rm output.sys
+
+fi
+srcfile=/blob/v-jinhzh/data/wmttest/testdata/test.${year}.${src}.tok
+for id in `seq 0 $enstopcnt`
+do
+    echo ">>> cat $srcfile >> input.tok "
+    cat $srcfile >> input.tok
+    echo ">>> wc -l  ensembleoutput.sys.$id"
+    wc -l  ensembleoutput.sys.$id
+    echo ">>> cat ensembleoutput.sys.$id >> output.sys"
+    cat ensembleoutput.sys.$id >> output.sys
+    echo ">>> rm ensembleoutput.sys.$id"
+    rm ensembleoutput.sys.$id
+done
+
+echo "echo wc -l output.sys"
+wc -l output.sys
+
+for id in `seq 0 $stopcnt`
+do
+    echo ">>> bash multieval.sh ${ckts[$id]} ${datapaths[$id]} ${bpetcs[$id]} $year $src  $lenpen $beamsize $tgt"
+    bash multieval.sh ${ckts[$id]} ${datapaths[$id]} ${bpetcs[$id]} $year $src  $lenpen $beamsize $tgt
+    echo ">>> mv output.score output.score.${id}"
+    mv output.score output.score.${id}
+    echo ">>> wc -l output.score.${id}"
+    wc -l output.score.${id}
+    echo ">>> rm input.tok.bpe"
+    rm input.tok.bpe
+    echo ">>> rm output.sys.bpe"
+    rm output.sys.bpe
+done
+if [ -f output.score ]
+then
+    echo ">>> rm output.score"
+    rm output.score
+fi
+for id in `seq 0 $stopcnt`
+do
+    echo ">>> cat output.score.${id} >> output.score"
+    cat output.score.${id} >> output.score
+    echo ">>> rm output.score.${id}"
+    rm output.score.${id}
+done
+echo "wc -l output.score"
+wc -l output.score
+python encal.py $beamsize $ensembleid
+echo ">>> perl ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $tgt < output.tok > output.tok.detok"
+perl ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $tgt < output.tok > output.tok.detok
+echo ">>> cat output.tok.detok | ../sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t wmt${year:2:2} -l ${src}-${tgt}"
+ cat output.tok.detok | ../sockeye/sockeye_contrib/sacrebleu/sacrebleu.py -t wmt${year:2:2} -l ${src}-${tgt}
+rm input.tok
+rm output.sys
+rm output.score
